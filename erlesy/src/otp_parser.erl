83f8f333666ca67ce3eea2d1a5ff81dc843eaf46
ruanpienaar <ruan800@gmail.com>
2016-12-13 17:13:51 +0000

    Change .txt extension to .gv for DOT/Graphviz. Use a more specific Variable name :)

--- erlesy/src/otp_parser.erl
+++ erlesy/src/otp_parser.erl
@@ -23,8 +23,8 @@
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-create_graph(FileName, IncludeFiles, Mode)->
-  gen_server:cast(?MODULE, {create, FileName, IncludeFiles, Mode}).
+create_graph(FileName, IncludePaths, Mode)->
+  gen_server:cast(?MODULE, {create, FileName, IncludePaths, Mode}).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -36,20 +36,20 @@ handle_call(_Request, _From, State) ->
   Reply = ok,
   {reply, Reply, State}.
 
-handle_cast({create, FileName, IncludeFiles, json}, State) ->
-  {parsed, _, Digraph} = graph_builder:parse_file(FileName, IncludeFiles),
+handle_cast({create, FileName, IncludePaths, json}, State) ->
+  {parsed, _, Digraph} = graph_builder:parse_file(FileName, IncludePaths),
   Output = erlesy_json_builder:encode(Digraph),
   erlesy_fs:write_output(filename:rootname(FileName) ++ erlesy_json_builder:extension(), Output),
   {noreply, State};
-handle_cast({create, FileName, IncludeFiles, dot}, State) ->
-  {ok, File} = file:open(filename:rootname(FileName) ++ ".txt", [write]),
-  {parsed, _, Digraph} = graph_builder:parse_file(FileName, IncludeFiles),
+handle_cast({create, FileName, IncludePaths, dot}, State) ->
+  {ok, File} = file:open(filename:rootname(FileName) ++ ".gv", [write]),
+  {parsed, _, Digraph} = graph_builder:parse_file(FileName, IncludePaths),
   file:write(File, dot:digraph_to_dot(filename:rootname(FileName), Digraph)),
   file:close(File),
   {noreply, State};
-handle_cast({create, FileName, IncludeFiles, plantuml}, State) ->
+handle_cast({create, FileName, IncludePaths, plantuml}, State) ->
   {ok, File} = file:open(filename:rootname(FileName) ++ ".txt", [write]),
-  {parsed, _, Digraph} = graph_builder:parse_file(FileName, IncludeFiles),
+  {parsed, _, Digraph} = graph_builder:parse_file(FileName, IncludePaths),
   file:write(File, dot:digraph_to_plantuml(filename:rootname(FileName), Digraph)),
   file:close(File),
   {noreply, State}.
